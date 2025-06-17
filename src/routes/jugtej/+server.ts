c891a62b7f07e76c8df74dd20b13f8d525f91909
Jugtej <jugtej.singh@sunderlandsoftwarecity.com>
2025-06-17 15:53:32 +0100

    Added in actor server

--- src/routes/jugtej/+server.ts
+++ src/routes/jugtej/+server.ts
@@ -2,30 +2,38 @@ import {json} from "@sveltejs/kit";
 
 export function GET() {
     return json(
-    {
-        "@context": "https://www.w3.org/ns/activitystreams",
-        "id": "https://getdigip.in/jugtej",
-        "type": "Person",
-        "following": "https://mastodon.jgarr.net/following",
-        "followers": "https://mastodon.jgarr.net/followers",
-        "inbox": "https://mastodon.jgarr.net/inbox",
-        "preferredUsername": "jugtej",
-        "name": "Jugtej Singh",
-        "summary": "Welcome to my website.",
-        "url": "https://getdigip.in/",
-        "manuallyApprovesFollowers": true,
-        "discoverable": true,
-        "published": "2000-01-01T00:00:00Z",
-
-        "icon": {
-        "type": "Image",
-            "mediaType": "image/jpeg",
-            "url": "https://mastodon.jgarr.net/icon.jpg"
-    },
-        "image": {
-        "type": "Image",
-            "mediaType": "image/jpeg",
-            "url": "https://mastodon.jgarr.net/image.png"
-    }
-    })
+        {
+            "@context": [
+                "https://www.w3.org/ns/activitystreams",
+                "https://w3id.org/security/v1"
+            ],
+            "id": "https://getdigip.in/jugtej",
+            "type": "Person",
+            "preferredUsername": "jugtej",
+            "name": "Jugtej Singh",
+            "summary": "Welcome to my website.",
+            "url": "https://getdigip.in/",
+            "published": "2000-01-01T00:00:00Z",
+            "manuallyApprovesFollowers": true,
+            "discoverable": true,
+            "inbox": "https://getdigip.in/jugtej/inbox",
+            "outbox": "https://getdigip.in/jugtej/outbox",
+            "followers": "https://getdigip.in/jugtej/followers",
+            "following": "https://getdigip.in/jugtej/following",
+            "icon": {
+                "type": "Image",
+                "mediaType": "image/jpeg",
+                "url": "https://getdigip.in/icon.jpg"
+            },
+            "image": {
+                "type": "Image",
+                "mediaType": "image/jpeg",
+                "url": "https://getdigip.in/image.png"
+            },
+            "publicKey": {
+                "id": "https://getdigip.in/jugtej#main-key",
+                "owner": "https://getdigip.in/jugtej",
+                "publicKeyPem": "-----BEGIN PUBLIC KEY-----\n...your key here...\n-----END PUBLIC KEY-----"
+            }
+        })
 }
