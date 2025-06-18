60a6989d7d35127a507ea726fcfb05a4746b406a
Jugtej <jugtej.singh@sunderlandsoftwarecity.com>
2025-06-18 11:27:36 +0100

    Older Changes

--- src/routes/jugtej/outbox/+server.ts
+++ src/routes/jugtej/outbox/+server.ts
@@ -3,7 +3,7 @@ import {json} from "@sveltejs/kit";
 export function GET() {
     const content = {
         "@context": "https://www.w3.org/ns/activitystreams",
-        "id": "https://maho.dev/socialweb/outbox",
+        "id": "https://getdigip.in/jugtej/outbox",
         "type": "OrderedCollection",
         "summary": "Bite-sized pieces of Software Engineering from a Garbage Code Connoisseur by Maho Pacheco",
         "totalItems": 24,
