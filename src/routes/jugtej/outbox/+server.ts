5ab35282eccca7398d2783e2b609148ea79312c9
Jugtej <jugtej.singh@sunderlandsoftwarecity.com>
2025-06-19 09:57:18 +0100

    Older Changes

--- src/routes/jugtej/outbox/+server.ts
+++ src/routes/jugtej/outbox/+server.ts
@@ -5,8 +5,25 @@ export function GET() {
         "@context": "https://www.w3.org/ns/activitystreams",
         "id": "https://getdigip.in/jugtej/outbox",
         "type": "OrderedCollection",
-        "summary": "Bite-sized pieces of Software Engineering from a Garbage Code Connoisseur by Maho Pacheco",
+        "summary": "Just my outbox",
         "totalItems": 24,
+        "orderedItems" : [
+            {
+                "@context": "https://www.w3.org/ns/activitystreams",
+                "id": "https://mastodon.example/users/bob#votes/827164/activity",
+                "to": "https://mastodon.example/users/alice",
+                "actor": "https://mastodon.example/users/bob",
+                "type": "Create",
+                "object": {
+                    "id": "https://getdigip.in/jugtej/blogs/blog1",
+                    "type": "Note",
+                    "name": "banana",
+                    "attributedTo": "https://mastodon.example/users/bob",
+                    "to": "https://mastodon.example/users/alice",
+                    "inReplyTo": "https://mastodon.example/users/alice/statuses/1009947848598745"
+                }
+            }
+        ]
     }
 
     const response = new Response(JSON.stringify(content));
