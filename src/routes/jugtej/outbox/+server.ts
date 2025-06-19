1cfb699083d782dfad46761a81b48a0af6ba85aa
Jugtej <jugtej.singh@sunderlandsoftwarecity.com>
2025-06-19 09:45:55 +0100

    Older Changes

--- src/routes/jugtej/outbox/+server.ts
+++ src/routes/jugtej/outbox/+server.ts
@@ -7,31 +7,9 @@ export function GET() {
         "type": "OrderedCollection",
         "summary": "Bite-sized pieces of Software Engineering from a Garbage Code Connoisseur by Maho Pacheco",
         "totalItems": 24,
-        "orderedItems": [
-            {
-                "@context": "https://www.w3.org/ns/activitystreams",
-                "id": "https://getdigip.in/jugtej/blogs/blog1/jsonFile",
-                "type": "Note",
-                "content": "The Gendered Lens of AI: Unpacking Bias in Language  ... html content",
-                "url": "https://getdigip.in/jugtej/blogs/blog1",
-                "attributedTo": "https://getdigip.in/jugtej/actor",
-                "to": [
-                    "https://www.w3.org/ns/activitystreams#Public"
-                ],
-                "cc": [],
-                "published": "2024-02-18T21:06:38-08:00",
-                "tag": [
-                    {
-                        "type": "Mention",
-                        "href": "https://getdigip.in/jugtej",
-                        "name": "@jugtej@getdigip.in"
-                    }
-                ]
-            }
-        ]
     }
 
-    const response = new Response(JSON.stringify(content), {headers: {'Content-Type': 'application/activity+json'}});
+    const response = new Response(JSON.stringify(content));
 
     return response;
 }
