9261b1c23b56dfd8bcb219f607284225335d9986
Jugtej <jugtej.singh@sunderlandsoftwarecity.com>
2025-06-19 10:33:42 +0100

    Older Changes

--- src/routes/jugtej/outbox/+server.ts
+++ src/routes/jugtej/outbox/+server.ts
@@ -9,29 +9,33 @@ export function GET() {
         "totalItems": 24,
         "orderedItems" : [
             {
+                // "@context": "https://www.w3.org/ns/activitystreams",
+                // "id": "https://getdigip.in/blog1.json",
+                // "type": "Note",
+                // "content": "The Gendered Lens of AI: Unpacking Bias in Language  ... html content",
+                // "url": "https://getdigip.in/jugtej/blogs/blog1",
+                // "attributedTo": "https://jugtej@getdigip.in",
+                // "to": [
+                //     "https://www.w3.org/ns/activitystreams#Public"
+                // ],
+                // "cc": [],
+                // "published": "2024-02-18T21:06:38-08:00",
+                // "tag": [
+                //     {
+                //         "Type": "Mention",
+                //         "Href": "https://hachyderm.io/users/mapache",
+                //         "Name": "@mapache@hachyderm.io"
+                //     },
+                //     {
+                //         "Type": "Hashtag",
+                //         "Href": "https://maho.dev/tags/ai",
+                //         "Name": "#ai"
+                //     },
+                // ],
                 "@context": "https://www.w3.org/ns/activitystreams",
-                "id": "https://getdigip.in/blog1.json",
                 "type": "Note",
-                "content": "The Gendered Lens of AI: Unpacking Bias in Language  ... html content",
-                "url": "https://getdigip.in/jugtej/blogs/blog1",
-                "attributedTo": "https://jugtej@getdigip.in",
-                "to": [
-                    "https://www.w3.org/ns/activitystreams#Public"
-                ],
-                "cc": [],
-                "published": "2024-02-18T21:06:38-08:00",
-                "tag": [
-                    {
-                        "Type": "Mention",
-                        "Href": "https://hachyderm.io/users/mapache",
-                        "Name": "@mapache@hachyderm.io"
-                    },
-                    {
-                        "Type": "Hashtag",
-                        "Href": "https://maho.dev/tags/ai",
-                        "Name": "#ai"
-                    },
-                ],
+                "name": "A Word of Warning",
+                "content": "Looks like it is going to rain today. Bring an umbrella!"
             }
         ]
     }
